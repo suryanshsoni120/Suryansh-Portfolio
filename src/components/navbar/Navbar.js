c32ee2b0b4ffbf15a8098dce2b9dd438f0a30962
Suryansh Soni <surisoni10@gmail.com>
2023-03-30 19:29:53 +0530

    Sidebar changes added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import { Link } from "react-scroll";
 import { FiMenu } from "react-icons/fi";
 import { MdClose, MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
-import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { FaGithub, FaTwitter, FaLinkedinIn } from "react-icons/fa";
 // import { logo } from "../../assets/index";
 import { icon } from "../../assets/index";
-//import { navLinksdata } from "../../constants";
+// import { navLinksdata } from "../../constants";
 
 const Navbar = (props) => {
   const [showMenu, setShowMenu] = useState(false);
@@ -103,25 +103,26 @@ const Navbar = (props) => {
               )}
             </span>
           </ul>
+
           <span
             onClick={() => setShowMenu(!showMenu)}
-            className="text-xl mdl:hidden bg-black w-10 h-10 inline-flex items-center justify-center rounded-full text-designColor cursor-pointer"
+            className="text-xl mdl:hidden bg-bodyColor dark:bg-white w-10 h-10 inline-flex items-center justify-center rounded-full text-designColor cursor-pointer"
           >
             <FiMenu />
           </span>
           {showMenu && (
-            <div className="w-[80%] h-screen overflow-scroll absolute top-0 left-0 bg-gray-900 p-4 scrollbar-hide">
+            <div className="w-[80%] h-screen overflow-scroll absolute top-0 left-0 bg-bodyColor dark:bg-white p-4 scrollbar-hide">
               <div className="flex flex-col gap-8 py-2 relative">
                 <div>
-                  {/* <img className="w-32" src={icon} alt="icon" /> */}
-                  <p className="text-sm text-gray-400 mt-2">
+                  <img className="w-22 py-2" src={icon} alt="icon" />
+                  {/* <p className="text-sm text-gray-400 mt-2">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                     Earum soluta perspiciatis molestias enim cum repellat,
                     magnam exercitationem distinctio aliquid nam.
-                  </p>
+                  </p> */}
                 </div>
-                {/* <ul className="flex flex-col gap-4">
-                  {navLinksdata.map((item) => (
+                <ul className="flex flex-col gap-4">
+                  {/* {navLinksdata.map((item) => (
                     <li
                       key={item._id}
                       className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
@@ -138,21 +139,91 @@ const Navbar = (props) => {
                         {item.title}
                       </Link>
                     </li>
-                  ))}
-                </ul> */}
+                  ))} */}
+                  <li className="text-base font-normal text-gray-400 dark:text-slate-400 tracking-wide cursor-pointer hover:text-designColor dark:hover:text-designColor duration-300">
+                    <Link
+                      spy={true}
+                      smooth={true}
+                      offset={-70}
+                      duration={500}
+                      className="nav-link"
+                      activeClass="active"
+                      to="home"
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li className="text-base font-normal text-gray-400 dark:text-slate-400 tracking-wide cursor-pointer hover:text-designColor dark:hover:text-designColor duration-300">
+                    <Link
+                      spy={true}
+                      smooth={true}
+                      offset={-70}
+                      duration={500}
+                      className="nav-link"
+                      activeClass="active"
+                      to="skills"
+                    >
+                      Skills
+                    </Link>
+                  </li>
+                  <li className="text-base font-normal text-gray-400 dark:text-slate-400 tracking-wide cursor-pointer hover:text-designColor dark:hover:text-designColor duration-300">
+                    <Link
+                      spy={true}
+                      smooth={true}
+                      offset={-70}
+                      duration={500}
+                      className="nav-link"
+                      activeClass="active"
+                      to="projects"
+                    >
+                      Projects
+                    </Link>
+                  </li>
+                  <li className="text-base font-normal text-gray-400 dark:text-slate-400 tracking-wide cursor-pointer hover:text-designColor dark:hover:text-designColor duration-300">
+                    <Link
+                      spy={true}
+                      smooth={true}
+                      offset={-70}
+                      duration={500}
+                      className="nav-link"
+                      activeClass="active"
+                      to="contact"
+                    >
+                      Contact
+                    </Link>
+                  </li>
+                </ul>
                 <div className="flex flex-col gap-4">
-                  <h2 className="text-base uppercase font-titleFont mb-4">
-                    Find me in
+                  <h2 className="text-base uppercase font-titleFont mb-4 dark:text-black">
+                    Find me on
                   </h2>
                   <div className="flex gap-4">
                     <span className="bannerIcon">
-                      <FaFacebookF />
+                      <a
+                        href="https://github.com/suryanshsoni120"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <FaGithub />
+                      </a>
                     </span>
                     <span className="bannerIcon">
-                      <FaTwitter />
+                      <a
+                        href="https://twitter.com/surisoni10"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <FaTwitter />
+                      </a>
                     </span>
                     <span className="bannerIcon">
-                      <FaLinkedinIn />
+                      <a
+                        href="https://www.linkedin.com/in/surisoni/"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <FaLinkedinIn />
+                      </a>
                     </span>
                   </div>
                 </div>
